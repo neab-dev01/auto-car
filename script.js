f1db4dca0781e61efbf2a67ae69a95d784c1d922
neab-dev01 <omgdigital.developer.01@gmail.com>
2025-12-02 01:28:05 +0700

    back to top

--- script.js
+++ script.js
@@ -51,3 +51,21 @@ window.addEventListener('scroll', () => {
         }
     });
 });
+
+// Back to Top Button
+const backToTopBtn = document.getElementById('backToTopBtn');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 300) {
+        backToTopBtn.classList.add('show');
+    } else {
+        backToTopBtn.classList.remove('show');
+    }
+});
+
+backToTopBtn.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
